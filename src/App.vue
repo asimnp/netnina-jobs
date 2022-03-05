441acff4788d3c085d9ee6d330a0c756208a9065
Asim Shrestha <asimshrestha608@gmail.com>
2022-03-05 12:12:56 +0545

    redirect, go back & forward

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
     <router-link :to="{ name: 'about' }">About</router-link> |
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go Forward</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -31,5 +52,17 @@ nav a.router-link-exact-active {
   background: crimson;
   padding: 5px 10px;
   border-radius: 5px;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  font-weight: bold;
+  cursor: pointer;
+}
+button:hover {
+  background: #e2e4e7;
 }
 </style>
